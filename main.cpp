a1ea61ada5126f0040cf7b6883055b5b5ec5f4cd
Gibson <d.evsyukov02@gmail.com>
2024-04-26 20:14:32 +0300

    Console crypto refactor

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ProviderShellExtension_i.h"
-#include "main.h"
-ProviderShellExtensionModule _AtlModule;
-HINSTANCE hInstanceShellExtension;
-extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance , DWORD dwReason , LPVOID lpReserved)
-{
-	hInstanceShellExtension = hInstance;
-	return 	_AtlModule.DllMain(dwReason, lpReserved);
-}
